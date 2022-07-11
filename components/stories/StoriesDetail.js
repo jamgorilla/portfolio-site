689e62dbecbd7bb13abdc69e9a34ae9511085c2b
jamgorilla <jamesmurphy507@hotmail.com>
2022-07-11 10:46:18 +0100

    parallax scrolling added to stories detail page

--- components/stories/StoriesDetail.js
+++ components/stories/StoriesDetail.js
@@ -1,20 +1,45 @@
 
-import { Fragment } from 'react';
+import { Fragment, useEffect, useRef } from 'react';
 
 function StoriesDetail(props) {
+
+    //const parallax = document.getElementById('detail-page-main-content');
+
+    const pageContainer = useRef(null);
+    
+    console.log("before", pageContainer)
+    
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            let offset = window.pageYOffset;
+            if (pageContainer.current === null) {
+                console.log("return", pageContainer)
+                
+                return;
+            } else {
+                console.log("run", pageContainer)
+
+                pageContainer.current.style.backgroundPositionY = offset*0.7 + 'px';
+             }
+        })
+    },[pageContainer])
+
     return (
         <Fragment>
-            <div className='gradient-overlay'>
+            {/* <div className='gradient-overlay'>
                 <img className="main-picture" src={ props.image } alt={ props.title } />
-            </div>
+            </div> */}
         
-        <div className='detail-page-main-content'>
-            <title>{ props.title }</title>
-            <h1>{ props.title }</h1>
-            <p>{ props.author }</p>
-            <p>{ props.content }</p>
+        <div className='white-div'></div>
+        <div style={{ backgroundImage: `url(${ props.image })` }} ref={ pageContainer } className='detail-page-main-content'>
+            <div className='text-holding-div'>
+                <title>{ props.title }</title>
+                <h1>{ props.title }</h1>
+                <p>{ props.author }</p>
+                <p>{ props.content }</p>
+            </div>
         </div>
-        <div className='spacer'></div>
+        
         </Fragment>
     );
 }
